5d18e08bfb87e80df7db89f3e69386c0916d0cdb
Asgeir Storesund Nilsen <asgeir@fintlabs.no>
2020-10-25 16:04:57 +0100

    Version 3.6.0 (#1)
    * Version 3.6.0.
    
    * Fixed HttpStatus handling in StatusCache
    
    * Model 3.6.10
    
    * Version 3.6.10

--- src/main/java/no/fint/consumer/config/LinkMapper.java
+++ src/main/java/no/fint/consumer/config/LinkMapper.java
@@ -8,13 +8,17 @@ import no.fint.model.personvern.samtykke.*;
 
 public class LinkMapper {
 
-	public static Map<String, String> linkMapper(String contextPath) {
-		return ImmutableMap.<String,String>builder()
-			.put(Behandling.class.getName(), contextPath + RestEndpoints.BEHANDLING)
-			.put(Samtykke.class.getName(), contextPath + RestEndpoints.SAMTYKKE)
-			.put(Tjeneste.class.getName(), contextPath + RestEndpoints.TJENESTE)
-			/* .put(TODO,TODO) */
-			.build();
-	}
+    public static Map<String, String> linkMapper(String contextPath) {
+        return ImmutableMap.<String,String>builder()
+            .put(Behandling.class.getName(), contextPath + RestEndpoints.BEHANDLING)
+            .put(Samtykke.class.getName(), contextPath + RestEndpoints.SAMTYKKE)
+            .put(Tjeneste.class.getName(), contextPath + RestEndpoints.TJENESTE)
+            .put("no.fint.model.personvern.kodeverk.Behandlingsgrunnlag", "/personvern/kodeverk/behandlingsgrunnlag")
+            .put("no.fint.model.personvern.kodeverk.Personopplysning", "/personvern/kodeverk/personopplysning")
+            .put("no.fint.model.felles.Person", "/felles/person")
+            .put("no.fint.model.administrasjon.organisasjon.Organisasjonselement", "/administrasjon/organisasjon/organisasjonselement")
+            /* .put(TODO,TODO) */
+            .build();
+    }
 
 }
--- src/main/java/no/fint/consumer/models/samtykke/SamtykkeCacheService.java
+++ src/main/java/no/fint/consumer/models/samtykke/SamtykkeCacheService.java
@@ -13,7 +13,6 @@ import no.fint.consumer.config.ConsumerProps;
 import no.fint.consumer.event.ConsumerEventUtil;
 import no.fint.event.model.Event;
 import no.fint.event.model.ResponseStatus;
-import no.fint.model.felles.kompleksedatatyper.Identifikator;
 import no.fint.relations.FintResourceCompatibility;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +29,7 @@ import java.util.stream.Collectors;
 import no.fint.model.personvern.samtykke.Samtykke;
 import no.fint.model.resource.personvern.samtykke.SamtykkeResource;
 import no.fint.model.personvern.samtykke.SamtykkeActions;
+import no.fint.model.felles.kompleksedatatyper.Identifikator;
 
 @Slf4j
 @Service
